c44f79563f767c48308305830cfcb154644fe1e5
Leo Ertuna <leo.ertuna@gmail.com>
2018-05-28 21:13:15 +0300

    Fixed hello world command

--- src/main/java/io/github/jpleorx/discordbot/command/HelloWorldCommand.java
+++ src/main/java/io/github/jpleorx/discordbot/command/HelloWorldCommand.java
@@ -1,11 +1,7 @@
 package io.github.jpleorx.discordbot.command;
 
-import io.github.jpleorx.discordbot.command.core.command.Command;
-import io.github.jpleorx.discordbot.command.core.reply.Reply;
-import io.github.jpleorx.discordbot.command.core.reply.TextReply;
-import org.javacord.api.event.message.MessageCreateEvent;
-
-import java.util.List;
+import io.github.jpleorx.discordbot.command.core.Placeholders;
+import io.github.jpleorx.discordbot.command.core.command.TextCommand;
 
 /**
  * Simple hello world command
@@ -13,27 +9,19 @@ import java.util.List;
  * @author Leo Ertuna
  * @since 28.05.2018 20:46
  */
-public class HelloWorldCommand implements Command {
-    private static final String command = "=привет";
-    private static final String replyMessage = "Привет пидор";
-
+public class HelloWorldCommand implements TextCommand {
     @Override
     public String getCommand() {
-        return command;
+        return "=привет";
     }
 
     @Override
-    public Reply getReply(MessageCreateEvent event) {
-        long authorId = event.getMessage().getAuthor().getId();
-        String authorMention = "<@" + authorId + ">";
-        String fullReply = replyMessage + " " + authorMention + "!";
-        return new TextReply(fullReply);
+    public String getMessageNoArguments() {
+        return "Ну привет-привет, пидрила, " + Placeholders.USER_MENTION + ".";
     }
 
     @Override
-    public Reply getReply(MessageCreateEvent event, List<String> args) {
-        String userMention = args.get(0);
-        String fullReply = replyMessage + " " + userMention + "!";
-        return new TextReply(fullReply);
+    public String getMessageFullArguments() {
+        return "Здарова " + Placeholders.USER_MENTION + "! Ты пидор, держу в курсе.";
     }
 }
