91818e22ba3fdf6e221a7ae6d5e202291f68b942
Leo Ertuna <leo.ertuna@gmail.com>
2018-05-28 21:11:49 +0300

    Introducing placeholders

---
+++ src/main/java/io/github/jpleorx/discordbot/command/core/Placeholders.java
@@ -0,0 +1,11 @@
+package io.github.jpleorx.discordbot.command.core;
+
+/**
+ * This will be a storage of text placeholders
+ *
+ * @author Leo Ertuna
+ * @since 28.05.2018 21:10
+ */
+public interface Placeholders {
+    public static final String USER_MENTION = "${userId}";
+}
