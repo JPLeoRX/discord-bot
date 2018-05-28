b71bdb66f8433028322089e01cb941dfdd36fa6b
Leo Ertuna <leo.ertuna@gmail.com>
2018-05-28 20:46:40 +0300

    Reworked hello world command

--- src/main/java/io/github/jpleorx/discordbot/command/HelloWorldCommand.java
+++ src/main/java/io/github/jpleorx/discordbot/command/HelloWorldCommand.java
@@ -1,13 +1,39 @@
 package io.github.jpleorx.discordbot.command;
 
+import io.github.jpleorx.discordbot.command.core.command.Command;
+import io.github.jpleorx.discordbot.command.core.reply.Reply;
+import io.github.jpleorx.discordbot.command.core.reply.TextReply;
 import org.javacord.api.event.message.MessageCreateEvent;
-import org.javacord.api.listener.message.MessageCreateListener;
 
-public class HelloWorldCommand implements MessageCreateListener {
+import java.util.List;
+
+/**
+ * Simple hello world command
+ *
+ * @author Leo Ertuna
+ * @since 28.05.2018 20:46
+ */
+public class HelloWorldCommand implements Command {
+    private static final String command = "=привет";
+    private static final String replyMessage = "Привет пидор";
+
     @Override
-    public void onMessageCreate(MessageCreateEvent messageCreateEvent) {
-        if (messageCreateEvent.getMessage().getContent().equalsIgnoreCase("!sayHi")) {
-            messageCreateEvent.getChannel().sendMessage("Hello " + messageCreateEvent.getMessage().getAuthor().getDisplayName() + "!");
-        }
+    public String getCommand() {
+        return command;
+    }
+
+    @Override
+    public Reply getReply(MessageCreateEvent event) {
+        long authorId = event.getMessage().getAuthor().getId();
+        String authorMention = "<@" + authorId + ">";
+        String fullReply = replyMessage + " " + authorMention + "!";
+        return new TextReply(fullReply);
+    }
+
+    @Override
+    public Reply getReply(MessageCreateEvent event, List<String> args) {
+        String userMention = args.get(0);
+        String fullReply = replyMessage + " " + userMention + "!";
+        return new TextReply(fullReply);
     }
 }
